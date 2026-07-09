7015e268f0c317a76059a266a6b48b4de5b3889b
Amadio336 <Amadio.lombardizzi@gmail.com>
2026-07-09 12:26:49 +0000

    risolti problemi di cors

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # 1. Definisci la tua "Lista VIP" (le origini del tuo frontend)
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:5501", 
+    "http://127.0.0.1:5500", 
     "http://192.168.1.15:80"
 ]
 
